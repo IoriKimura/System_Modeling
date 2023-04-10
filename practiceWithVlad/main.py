d7dd9295f69c7bb05f592ebd590d58a45f24ec72
IoriKimura <sergeykonstwork@gmail.com>
2023-04-10 18:15:01 +0300

    upd

---
+++ practiceWithVlad/main.py
@@ -0,0 +1,10 @@
+def main(s):
+    if not s:
+        return "Fuck you"
+    else:
+        return "Cock"
+
+
+if __name__ == '__main__':
+    string = str(input("Введите что-то"))
+    print(main(string))
